00ac11a80b78f633302567118c476904125b616b
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-04-20 21:40:58 -0300

    Add project

---
+++ ThreadsCloneSUI/ThreadsCloneSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneSUIApp.swift
+//  ThreadsCloneSUI
+//
+//  Created by Dmitry Volkov on 20/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
