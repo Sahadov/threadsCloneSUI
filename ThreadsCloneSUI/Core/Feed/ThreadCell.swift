64d02ab4abdd838591db6161350853d94b9f90cc
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-05-29 21:26:39 -0500

    Add ThreadCell

---
+++ ThreadsCloneSUI/Core/Feed/ThreadCell.swift
@@ -0,0 +1,79 @@
+//
+//  ThreadCell.swift
+//  ThreadsCloneSUI
+//
+//  Created by Dmitry Volkov on 29/05/2025.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                Image("AvatarImage")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("steve_here")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundStyle(Color(.systemGray3))
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundStyle(Color(.darkGray))
+                        }
+                    }
+                    
+                    Text("New app is released")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .foregroundStyle(.black)
+                    .padding(.vertical, 8)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
