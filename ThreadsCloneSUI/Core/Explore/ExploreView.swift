9e4520c5ae3cfd42028057cf5189f57f6506d1b6
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-05-29 21:56:26 -0500

    Add ExploreView

---
+++ ThreadsCloneSUI/Core/Explore/ExploreView.swift
@@ -0,0 +1,35 @@
+//
+//  ExploreView.swift
+//  ThreadsCloneSUI
+//
+//  Created by Dmitry Volkov on 29/05/2025.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    @State private var searchText: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0 ... 10, id: \.self) { user in
+                        VStack {
+                            UserCell()
+                            
+                            Divider()
+                        }
+                        .padding(.vertical, 4)
+                    }
+                }
+            }
+            .navigationTitle("Search")
+            .searchable(text: $searchText, prompt: "Search")
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
