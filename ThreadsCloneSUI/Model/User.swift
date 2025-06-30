144f85d60746f2a08fcd41b72592ad44438b5f04
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-06-30 12:53:23 -0500

    Add user model

---
+++ ThreadsCloneSUI/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  ThreadsCloneSUI
+//
+//  Created by Dmitry Volkov on 30/06/2025.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    let fullname: String
+    let email: String
+    let username: String
+    let profileImageUrl: String?
+    let bio: String?
+}
