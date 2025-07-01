5cd06fdbb11a050384db4c7f5e9ee29b3df2e237
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-06-30 23:30:04 -0500

    Add CurrentProfileView

---
+++ ThreadsCloneSUI/Core/Profile/CurrentUserProfile/View/CurrentUserProfileView.swift
@@ -0,0 +1,114 @@
+//
+//  CurrentUserProfileView.swift
+//  ThreadsCloneSUI
+//
+//  Created by Dmitry Volkov on 30/06/2025.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    @StateObject var viewModel = CurrentUserProfileViewModel()
+    @State private var selectedFilter: ProfileThreadFilter = .threads
+    @Namespace var animation
+    
+    private var currentUser: User? {
+        return viewModel.currentUser
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 20) {
+                    HStack(alignment: .top) {
+                        VStack(alignment: .leading, spacing: 12) {
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text(currentUser?.fullname ?? "")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                                
+                                Text(currentUser?.username ?? "")
+                                    .font(.subheadline)
+                            }
+                            
+                            if let bio = currentUser?.bio {
+                                Text(bio)
+                                    .font(.footnote)
+                            }
+                            
+                            Text("2 followers")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                        
+                        Spacer()
+                        
+                        CircularProfileImageView()
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Follow")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                            .frame(width: 352, height: 32)
+                            .background(.black)
+                            .cornerRadius(8)
+                    }
+                    
+                    // user content list view
+                    VStack {
+                        HStack {
+                            ForEach(ProfileThreadFilter.allCases) {filter in
+                                VStack {
+                                    Text(filter.title)
+                                        .font(.subheadline)
+                                        .fontWeight(selectedFilter == filter ? .semibold : .regular)
+                                    
+                                    if selectedFilter == filter {
+                                        Rectangle()
+                                            .foregroundStyle(.black)
+                                            .frame(width: 180, height: 1)
+                                            .matchedGeometryEffect(id: "item", in: animation)
+                                    } else {
+                                        Rectangle()
+                                            .foregroundStyle(.clear)
+                                            .frame(width: 180, height: 1)
+                                    }
+                                }
+                                .onTapGesture {
+                                    withAnimation(.spring()) {
+                                        selectedFilter = filter
+                                    }
+                                }
+                            }
+                        }
+                        
+                        LazyVStack {
+                            ForEach(0 ... 10, id: \.self) { thread in
+                                ThreadCell()
+                            }
+                        }
+                    }
+                    .padding(.vertical, 8)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        AuthService.shared.signOut()
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                    }
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+#Preview {
+    CurrentUserProfileView()
+}
