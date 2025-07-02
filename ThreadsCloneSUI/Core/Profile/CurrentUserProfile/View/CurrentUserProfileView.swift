6e79c6411e66708214ba46338ae4486fae875d0b
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-07-01 22:44:04 -0500

    Add PhotoPicker

--- ThreadsCloneSUI/Core/Profile/CurrentUserProfile/View/CurrentUserProfileView.swift
+++ ThreadsCloneSUI/Core/Profile/CurrentUserProfile/View/CurrentUserProfileView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CurrentUserProfileView: View {
     @StateObject var viewModel = CurrentUserProfileViewModel()
+    @State private var showEditProfile = false
     
     private var currentUser: User? {
         return viewModel.currentUser
@@ -21,7 +22,7 @@ struct CurrentUserProfileView: View {
                     ProfileHeaderView(user: currentUser)
                     
                     Button {
-                        
+                        showEditProfile.toggle()
                     } label: {
                         Text("Edit Profile")
                             .font(.subheadline)
@@ -39,6 +40,10 @@ struct CurrentUserProfileView: View {
                     UserContentListView()
                 }
             }
+            .sheet(isPresented: $showEditProfile, content: {
+                EditProfileView()
+                    .environmentObject(viewModel)
+            })
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button {
--- ThreadsCloneSUI/Core/Profile/CurrentUserProfile/ViewModel/CurrentUserProfileViewModel.swift
+++ ThreadsCloneSUI/Core/Profile/CurrentUserProfile/ViewModel/CurrentUserProfileViewModel.swift
@@ -7,9 +7,15 @@
 
 import Foundation
 import Combine
+import PhotosUI
+import SwiftUI
 
 class CurrentUserProfileViewModel: ObservableObject {
     @Published var currentUser: User?
+    @Published var selectedItem: PhotosPickerItem? {
+        didSet { Task { await loadImage() } }
+    }
+    @Published var profileImage: Image?
     var cancellables = Set<AnyCancellable>()
     
     init() {
@@ -19,8 +25,16 @@ class CurrentUserProfileViewModel: ObservableObject {
     private func setupSubscribers() {
         UserService.shared.$currentUser.sink { [weak self] user in
             self?.currentUser = user
-            print("DEBUG: User in view model from combine is \(self?.currentUser?.id)")
         }
         .store(in: &cancellables)
+    }
+    
+    @MainActor
+    private func loadImage() async {
+        guard let item = selectedItem else { return }
+        
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: data) else { return }
+        self.profileImage = Image(uiImage: uiImage)
     }
 }
